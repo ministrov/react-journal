7ca378f53f2bc201b3c4d6d66ec99c260ea93523
ministrov <61511030+ministrov@users.noreply.github.com>
2023-09-05 18:30:57 +0300

    feat: start operating on project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Button from './components/Button/Button';
+import JournalItem from './components/JournalItem/JournalItem';
+
 function App() {
 
   return (
-    <div>New Project</div>
+    <div>
+      New Project
+
+      <Button/>
+
+      <JournalItem/>
+    </div>
   );
 }
 
