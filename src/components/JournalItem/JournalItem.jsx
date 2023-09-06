44820e68eb304ad61c40f831bf39470754fa08b8
ministrov <61511030+ministrov@users.noreply.github.com>
2023-09-06 10:23:58 +0300

    feat: get props to the component

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,12 +1,13 @@
 import './JournalItem.css';
 
-function JournalItem() {
+function JournalItem({ title, text, date }) {
+
   return (
     <div className='journal-item'>
-      <h2 className='journal-item__header'>header</h2>
+      <h2 className='journal-item__header'>{title}</h2>
       <div className='journal-item__body'>
-        <div className='journal-item__date'>1</div>
-        <div className='journal-item__text'>2</div>
+        <div className='journal-item__date'>{date.toString()}</div>
+        <div className='journal-item__text'>{text}</div>
       </div>
     </div>
   );
