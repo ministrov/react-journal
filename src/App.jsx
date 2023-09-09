22000206c527926a5bacc6bb51bf6322f042bca5
ministrov <61511030+ministrov@users.noreply.github.com>
2023-09-09 12:38:57 +0300

    feat: operating on list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,60 @@
 import Body from './layouts/Body/Body';
 // import Button from './components/Button/Button';
-import CardButton from './components/CardButton/CardButton';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
-import JournalItem from './components/JournalItem/JournalItem';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 import Header from './components/Header/Header';
 import JournalList from './components/JournalList/JournalList';
 import './App.css';
 import JournalForm from './components/JournalForm/JournalForm';
+// import Button from './components/Button/Button';
+import { useState } from 'react';
+
+const INITIAL_DATA = [
+  // {
+  //   id: 1,
+  //   title: 'Подготовка к обновлению курсов',
+  //   text: 'Горные походы открывают удивительные природные ландшафты',
+  //   date: new Date()
+  // },
+  // {
+  //   id: 2,
+  //   title: 'Поход в годы',
+  //   text: 'Думал, что очень много времени',
+  //   date: new Date()
+  // },
+  // {
+  //   id: 3,
+  //   title: 'Первая заметка',
+  //   text: 'Создал первую заметку, чтобы',
+  //   date: new Date()
+  // }
+];
 
 function App() {
-  const data = [
-    {
-      title: 'Подготовка к обновлению курсов',
-      text: 'Горные походы открывают удивительные природные ландшафты',
-      date: new Date()
-    },
-    {
-      title: 'Поход в годы',
-      text: 'Думал, что очень много времени',
-      date: new Date()
-    },
-    {
-      title: 'Первая заметка',
-      text: 'Создал первую заметку, чтобы',
-      date: new Date()
-    }
-  ];
+  const [items, setItems] = useState(INITIAL_DATA);
+
+  const addItem = item => {
+    console.log(item);
+    setItems(oldItems => [...oldItems, {
+      text: item.text,
+      title: item.title,
+      date: new Date(item.date),
+      id: Math.max(...oldItems.map(i => i.id)) + 1
+    }]);
+  };
+
+  // console.log(addItem(items));
 
   return (
     <div className="app">
       <LeftPanel>
         <Header/>
         <JournalAddButton />
-        <JournalList>
-          <CardButton>
-            <JournalItem
-              title={data[0].title}
-              text={data[0].text}
-              date={data[0].date}
-            />
-          </CardButton>
-
-          <CardButton>
-            <JournalItem
-              title={data[1].title}
-              text={data[1].text}
-              date={data[1].date}
-            />
-          </CardButton>
-
-          <CardButton>
-            <JournalItem
-              title={data[2].title}
-              text={data[2].text}
-              date={data[2].date}
-            />
-          </CardButton>
-        </JournalList>
+        <JournalList items={items}/>
       </LeftPanel>
 
       <Body>
-        <JournalForm/>
+        <JournalForm onSubmit={addItem}/>
       </Body>
     </div>
   );
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,37 +1,27 @@
-import { useState } from 'react';
 import './JournalForm.css';
 import Button from '../Button/Button';
 
-function JournalForm() {
-  const [inputData, setInputData] = useState('');
-  const [state, setState] = useState({
-    age: 31
-  });
-
-  const handleInput = (event) => {
-    setInputData(event.target.value);
-    console.log(inputData);
-  };
+function JournalForm({ onSubmit }) {
 
   const addJournalItem = (event) => {
     event.preventDefault();
 
-    state.age = 40;
+    // state.age = 40;
 
-    setState({ ...state });
+    // setState({ ...state });
 
     const formData = new FormData(event.target);
     const formProps = Object.fromEntries(formData);
+    onSubmit(formProps);
     console.log(formProps);
   };
 
   return (
     <form className="journal-form" onSubmit={addJournalItem}>
-      {state.age}
       <input type="text" name="title"/>
       <input type="date" name="date"/>
-      <input type="text" name="tag" value={inputData} onChange={handleInput} />
-      <textarea name="post" id="" cols="30" rows="10"></textarea>
+      <input type="text" name="tag"/>
+      <textarea name="text" id="" cols="30" rows="10"></textarea>
       <Button text="Сохранить" onClick={() => { console.log('Clicked'); }}/>
     </form>
   );
