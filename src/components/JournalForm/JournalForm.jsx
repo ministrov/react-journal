5edc98cd68f64543fa76640f417b42b8ab8126d8
ministrov <61511030+ministrov@users.noreply.github.com>
2023-09-09 15:27:19 +0300

    feat: operating with classNames library

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
+import classNames from 'classnames';
 import Button from '../Button/Button';
-import './JournalForm.css';
+import styles from './JournalForm.module.css';
 
 function JournalForm({ onSubmit }) {
   const [formValidState, setFormValidState] = useState({
@@ -51,11 +52,13 @@ function JournalForm({ onSubmit }) {
   };
 
   return (
-    <form className="journal-form" onSubmit={addJournalItem}>
-      <input type="text" name="title" style={{ border: formValidState.title ? undefined : '1px solid red' }}/>
-      <input type="date" name="date" style={{ border: formValidState.date ? undefined : '1px solid red' }} />
+    <form className={styles['journal-form']} onSubmit={addJournalItem}>
+      <input type="text" name="title" className={classNames(styles['input'], {
+        [styles['invalid']]: !formValidState.title
+      })} />
+      <input type="date" name="date" className={`${styles['input']} ${formValidState.date ? '' : styles['invalid']}`} />
       <input type="text" name="tag"/>
-      <textarea name="text" id="" cols="30" rows="10" style={{ border: formValidState.text ? undefined : '1px solid red' }}></textarea>
+      <textarea name="text" id="" cols="30" rows="10" className={`${styles['input']} ${formValidState.text ? '' : styles['invalid']}`}></textarea>
       <Button text="Сохранить" />
     </form>
   );
