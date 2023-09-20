51d9c36c543c1c9540414261d4bb5aa712f7ca2c
ministrov <61511030+ministrov@users.noreply.github.com>
2023-09-20 11:05:46 +0300

    feat: start operating with memo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,12 @@
+import { memo } from 'react';
 import styles from './Logo.module.css';
 
+// eslint-disable-next-line react-refresh/only-export-components
 function Logo({ image }) {
   console.log('Logo');
-  
+
   return <img className={styles.logo} src={image} alt="Логотип журнала" />;
 }
 
-export default Logo;
+// eslint-disable-next-line react-refresh/only-export-components
+export default memo(Logo);
