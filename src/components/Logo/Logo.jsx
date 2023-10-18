e6033d55b80d6dec6bfcfb2501fadbca2cb3d658
ministrov <61511030+ministrov@users.noreply.github.com>
2023-10-18 20:16:07 +0300

    feat: make some hotfixes

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,8 @@
-import { memo } from 'react';
+// import { memo } from 'react';
 import styles from './Logo.module.css';
 
-// eslint-disable-next-line react-refresh/only-export-components
 function Logo({ image }) {
-  console.log('Logo');
-
   return <img className={styles.logo} src={image} alt="Логотип журнала" />;
 }
 
-// eslint-disable-next-line react-refresh/only-export-components
-export default memo(Logo);
+export default Logo;
